efd7fe9290692df7dd0d655c83bf548332a64ce9
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-03-03 18:37:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ log="dumb.log"
 
 chmod +x /
 rclone version
+cat /home/quota/init_h_login |sed "s|HEROKU_EMAIL_ID|$HEROKU_EMAIL_ID|g;s|HEROKU_PASSWORD|$HEROKU_PASSWORD|g" >/usr/bin/init_h_login
+chmod +x /usr/bin/init_h_login 
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes > "$log" 2>&1 &
 while sleep 1
